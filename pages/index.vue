c7caf3deb76f502da37e63787e7b20491cdeb259
yury.kryshchuk <yury.kryshchuk@exposit.com>
2017-12-24 23:13:02 +0300

    please

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,18 @@
 <template>
   <section class="container">
-   <div>lol</div>
+    <div>
+      <logo/>
+      <h1 class="title">
+        personal-calendar
+      </h1>
+      <h2 class="subtitle">
+        Nuxt.js project
+      </h2>
+      <div class="links">
+        <a href="https://nuxtjs.org/" target="_blank" class="button--green">Documentation</a>
+        <a href="https://github.com/nuxt/nuxt.js" target="_blank" class="button--grey">GitHub</a>
+      </div>
+    </div>
   </section>
 </template>
 
